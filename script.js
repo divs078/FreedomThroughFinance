c12ef5dd98aa0906fd79ad03e822b3a16475959e
Divya Sharma <122336623+divs078@users.noreply.github.com>
2023-02-05 00:50:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+$(':radio').change(function() {
+  console.log('New star rating: ' + this.value);
+});
+
+
+//var yes = $('.yes');
+//var mOInvest = $('.mOInvest');
+// var submitBtn = $('.submit-btn');
+
+
+// submitBtn.on("click", showResults);
+
+// function showResults () {
+// console.log(hi);
+
+// if (.yes = )
+
+//   if(document.getElementById('invest_yes').checked) {
+//   console.log(hi); 
+// } else if(document.getElementById('invest_no').checked) {
+//   console.log(bye);
+//   }
+// }
+   
+
+// I will comment out some of your code to try something really quick
+
+
+
+// console.log(hi);
+
+
+
+// if (.yes = )
+
+//   if(document.getElementById('invest_yes').checked) {
+//   console.log(hi); 
+// } else if(document.getElementById('invest_no').checked) {
+//   console.log(bye);
+//   }
+// }
